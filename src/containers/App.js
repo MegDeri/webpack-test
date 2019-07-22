3f19ca650f1df529ad39b4c95ab36b2a29905007
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-07-22 22:29:01 +0300

    Change app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state = {
-            data: []
+            data: [{
+                  number: 1,
+                  text: "Ola"
+                },
+                {
+                  number: 2,
+                  text: "Zosia"
+                },
+                {
+                  number: 3,
+                  text: "Kasia"
+                }
+            ]
         };
     }
+
     addTodo(val){
         const todo = {
             text: val,
@@ -17,14 +31,16 @@ class App extends React.Component {
         const data = [...this.state.data, todo];
         this.setState({data});
     }
+
     removeTodo(id) {
         const remainder = this.state.data.filter(todo => todo.id !== id);
         this.setState({data: remainder});
     }
+    
     render() {
         return (
             <div className={style.TodoApp}>
-                Tutaj pojawią się komponenty naszej aplikacji.
+                <Title title="Title in webpack" taskNumber={this.state.data.length}></Title>
             </div>
         );
     }
